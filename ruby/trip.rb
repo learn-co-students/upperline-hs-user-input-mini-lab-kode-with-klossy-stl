664a8eb335e93dcf990743ebcc08a7dee182d25a
Theresa Fister <tfister97@gmail.com>
2016-06-26 01:53:46 +0000

    Done.

--- ruby/trip.rb
+++ ruby/trip.rb
@@ -1,1 +1,11 @@
 #Code here!
+puts "Hello! Thank you for planning a trip to New York City! Where would you like to stay?".chomp
+place = gets
+puts "That's a great place! Now, where do you most want to visit?".chomp
+attraction = gets
+puts "Wonderful! Now, what food would you like to eat?".chomp
+food = gets
+puts "That sounds delicious! How many nights are you planning to stay in the lovely NYC?".chomp
+night = gets
+puts "What a perfect amount."
+puts "So, it looks like you will stay in new york for #{night}s, you will be staying at #{place}, you want to visit #{attraction}, and you will be eating #{food}! Thank you for planning your trip- I hope you have a wonderful one!".chomp
